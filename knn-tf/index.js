ff01f4516069e87df219175549bbef79bd297498
Walker Gusmão <walker@praiseweb.com.br>
2019-11-10 18:55:50 -0300

    add accuracy

--- knn-tf/index.js
+++ knn-tf/index.js
@@ -1,10 +1,10 @@
 require('@tensorflow/tfjs-node');
 const tf = require('@tensorflow/tfjs');
 const loadCsv = require('./load-csv');
-
+const splitTest = 10;
 const csv = loadCsv('kc_house_data.csv', {
     shuffle: true,
-    splitTest: 10,
+    splitTest,
     dataColumns: ['lat', 'long'],
     labelColumns: ['price'],
 });
@@ -13,8 +13,6 @@ const labels = tf.tensor(csv.labels);
 const testFeatures = tf.tensor(csv.testFeatures);
 const testLabels = tf.tensor(csv.testLabels);
 
-
-    
 function knn(testPoint, k) {
     // d = (d1² + d2²) ** 0.5
     // d = (fLat - lLat - fLon - lLon)² + ... ** 0.5
@@ -32,14 +30,17 @@ function knn(testPoint, k) {
             return t1d[0] - t2d[0]; // by distance
         })
         .slice(0, k)
-        .reduce((sum, tensor) => sum + tensor.dataSync()[1], 0) / k;
+        // nearist avarage label values
+        .reduce((sum, tensor) => sum + tensor.dataSync()[1], 0) / k; 
 }
 
 function accuracyOfKs(range = [1, 20]) {
     console.log('Accuracy of Ks...');
     const results = [...Array(range[1]).keys()].map(
-        k => knn()
-    )
+        k => testFeatures.dataSync().filter(
+            testPoint => knn(testPoint, k) == // TODO
+        )
+    );
     console.table(results);
 }
 
@@ -48,4 +49,5 @@ function predict(input) {
     console.log('prediction', prediction);
 }
 
-predict(tf.tensor([47.4231, -122.200]));
+accuracyOfKs();
+// predict(tf.tensor([47.4231, -122.200]));
